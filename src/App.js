9bffbc7c02beb8d42986ac806504f5fdb38fea72
webhero-nur <nurealom522@gmail.com>
2023-02-04 01:56:50 +0600

    blog class added to article tag inside JSX

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 function Article() {
   return (
     <div>
-      <article>
+      <article className='blog'>
         <h1>Title: </h1>
         <p>Body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae expedita labore similique rerum alias suscipit quam nihil officia! Debitis reprehenderit dolor accusantium odio pariatur provident, ullam fuga suscipit excepturi cupiditate incidunt, optio maiores esse vel quos voluptas iusto voluptatibus, earum ut porro dolorem? Obcaecati tenetur, consequuntur culpa earum beatae debitis.</p>
       </article>
