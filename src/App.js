e85e2bbe78dd3a7fedbc9dc89ac2614bc0ed5549
webhero-nur <nurealom522@gmail.com>
2023-02-04 02:24:25 +0600

    send property to every component and showed the property

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Blog from './components/Blog/Blog';
 
 function App() {
+  const [newHeading, newAuthor] = ['No shortcut to success', 'Billi Sharon']
   return (
     <div className="App">
       <Article></Article>
-      <Blog></Blog>
-      <Blog></Blog>
-      <Blog></Blog>
+      <Blog heading='My Sundarban Blog' author='MySelf'></Blog>
+      <Blog heading='Experience of ePassport in BD' author='Mr. Alief Hossain'></Blog>
+      <Blog heading={newHeading} author={newAuthor}></Blog>
     </div>
   );
 }
--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Blog.css'
 
-const Blog = () => {
+const Blog = (props) => {
     return (
         <div>
             <article className='blog'>
-                <h2>Title: </h2>
-                <h4>Author: </h4>
+                <h2>Title: {props.heading}</h2>
+                <h4>Author: {props.author}</h4>
                 <p>This is Bunny. He was funny. He likes honey.This is Bunny. He was funny. He likes honey.This is Bunny. He was funny. He likes honey.This is Bunny. He was funny. He likes honey.This is Bunny. He was funny. He likes honey. </p>
             </article>
         </div>
